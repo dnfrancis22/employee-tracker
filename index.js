27c8f2367f06afeaee00f87146fa5965280e9fef
Dennis Francis <dnfrancis22@gmail.com>
2020-12-07 09:01:16 -0500

    added connection to data base.

--- index.js
+++ index.js
@@ -22,3 +22,25 @@ connection.connect(function(err) {
   // run the start function after the connection is made to prompt the user
   start();
 });
+
+// function which prompts the user for what action they should take
+function start() {
+    inquirer
+      .prompt({
+        name: "postOrBid",
+        type: "list",
+        message: "Would you like to [POST] an auction or [BID] on an auction?",
+        choices: ["POST", "BID", "EXIT"]
+      })
+      .then(function(answer) {
+        // based on their answer, either call the bid or the post functions
+        if (answer.postOrBid === "POST") {
+          postAuction();
+        }
+        else if(answer.postOrBid === "BID") {
+          bidAuction();
+        } else{
+          connection.end();
+        }
+      });
+  }
